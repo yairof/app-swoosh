205836ec2aea67342302f741ec687966b5a62c21
Yairo Fernandez <Yairologo@Yairos-MacBook-Pro.local>
2018-10-19 16:08:59 -0400

    minor changes

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -9,13 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var bgImg: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+       // swoosh.frame = CGRect(x: view.frame.size.width / 2 - swoosh.frame.size.width / 2, y: 50, width: swoosh.frame.size.width, height: swoosh.frame.size.height)
+        // x is the horizontal line
+        // Every frame has a position and it has a size.
+        // view.frame.size.width this is the views width
+        // view.frame.size.height this is the views height
+        // I want to grab the width of the current device and devide it by two. That puts it right at the center.
+        // bgImg.frame = view.frame; makes image fit frame
     }
-
-    override func didReceiveMemoryWarning() {
+    
+        override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
