8e7e5a8bd8f34fa364d36885a48c5684f778f478
Yairo Fernandez <Yairologo@Yairos-MacBook-Pro.local>
2018-10-23 13:29:01 -0400

    added more functunality

--- app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -9,13 +9,40 @@
 import UIKit
 
 class LeagueVC: UIViewController {
+    
+    var player: Player!
 
+    @IBOutlet weak var nextBtn: BorderButton!
+ 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        player = Player()
+
+        // Do any additional setup after loading the view.
+    }
+    // created the segue for getting to the SkillVC and disabled the next button on storyboard
     @IBAction func onNextTapped(_ sender: Any) {
         performSegue(withIdentifier: "skillVCSegue", sender: self)
     }
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    
+    // created 3 ibactions to map a key pressed on the 3 buttons on VC
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectLeague(leagueType: "mens")
+    }
+    
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectLeague(leagueType: "womens")
 
-        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onCoedTapped(_ sender: Any) {
+        selectLeague(leagueType: "coed")
+    }
+    
+    // created a function to enable the button on keypress up above. Allowing the next button to become enabled again.
+    func selectLeague(leagueType: String) {
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true
     }
 }
---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,16 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Yairo Fernandez on 10/19/18.
+//  Copyright © 2018 Yairo Fernandez. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
+
+// Whenever you need to work with data put in a Struct or Class, never pass around multiple variables between view controllers.
